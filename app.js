1fa36bfe04090645743301e4655a15db61a7a050
kentguo_1997 <guoyunkai201709@gmail.com>
2022-02-15 20:52:41 +0800

    feat: beautify index, new and edit.handlebars

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 // Include packages in the project
 const express = require('express')
-const app = express()
-require('./config/mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+
 const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
 
 // Define server related variables
 const port = 3000
@@ -16,12 +18,11 @@ app.engine('handlebars', exphbs({ extname: 'handlebars', defaultLayout: 'main' }
 app.set('view engine', 'handlebars')
 
 
-// setting app use for included sources
+// setting app use for included packages
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(routes)
-
 
 
 // start and listen the server
