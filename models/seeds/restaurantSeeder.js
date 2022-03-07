723a0a686b3aeeb231c9f679ed1f53d29226b640
kentguo_1997 <guoyunkai201709@gmail.com>
2022-03-07 16:02:28 +0800

    refactor: modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,25 +1,56 @@
-// Include mongoose.js & Restaurant Model & restaurant.json
+// For using MONGODB_URI from .env in mongoose.js
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// Include mongoose.js & Models & restaurant.json
 const db = require('../../config/mongoose')
 const Restaurant = require('../restaurant')
+const User = require('../user') 
 const restaurantsList = require('../../restaurant.json')
 
+// Include bcryptjs
+const bcrypt = require('bcryptjs')
 
+// Define seed users
+const SEED_USERS = [
+  {
+    name: 'seedUser1', 
+    email: 'user1@example.com',
+    password: '12345678', 
+    ownedRestaurants: restaurantsList.results.slice(0, 3)
+
+  }, 
+  {
+    name: 'seedUser2',
+    email: 'user2@example.com',
+    password: '12345678',
+    ownedRestaurants: restaurantsList.results.slice(3)
+  }
+]
+
+// create data
 db.once('open', () => {
-  restaurantsList.results.forEach( 
-    restaurant => Restaurant.create({ 
-      id: restaurant.id,
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description
-    })
-  )
-
-  console.log('done')
+  // Create User
+  Promise.all(Array.from(SEED_USERS, seedUser => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(seedUser.password, salt))
+      .then(hash => User.create({
+        name: seedUser.name,
+        email: seedUser.email,
+        password: hash
+      }))
+      .then( user => {
+        const userId = user._id
+        seedUser.ownedRestaurants.forEach(restaurant => {
+          restaurant['userId'] = userId
+        })
+        return Restaurant.create(seedUser.ownedRestaurants)
+      })
+  })).then(() => {
+    console.log('done')
+    process.exit()
+  })
 })
 
